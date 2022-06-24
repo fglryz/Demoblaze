700b790f6d90f75e3ed05892901393581dbdbc3f
Fatma Guleryuz <fglryz.111@gmail.com>
2022-06-24 11:35:58 +0100

    read again

--- src/test/java/com/demoblaze/runners/CukesRunner.java
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "jason:target/cucumber.json",
+                "json:target/cucumber.json",
                 "html:target/cucumber-report.html",
                 "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt",
